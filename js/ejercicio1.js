3519785739676297958dcd13255ef293058575ea
Juan Menichetti <jpmenichetti13@gmail.com>
2022-06-01 19:52:44 -0300

    se agrego el ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,2 +1,17 @@
 // Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
 
+let auto = {
+    color: "blanco",
+    marca: "Ford",
+    modelo: "Ranger",
+    encendido: true,
+    encenderlo:() => {
+        document.write("Ingrese la llave y girarla");
+    }
+}
+
+console.log(auto);
+
+auto.encendido = false
+document.write(`<br> encendido: ${auto.encendido}`);
+
